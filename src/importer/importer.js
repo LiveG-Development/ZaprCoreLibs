64676bde64596cf4a897782e2b699d477d0aa5b3
James-Livesey <jamesliveg@gmail.com>
2019-06-05 18:58:06 +0100

    Add importer library to allow for easy asset importing

---
+++ src/importer/importer.js
@@ -0,0 +1,42 @@
+// Zapr Core Libraries
+// 
+// Copyright (C) LiveG. All Rights Reserved.
+// Copying is not a victimless crime. Anyone caught copying LiveG software may
+// face sanctions.
+// 
+// https://liveg.tech
+// Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+
+// @import ../dom/dom
+
+var importer = {
+    /*
+        @name importer.generateLink
+
+        @param asset string Asset contents to use in Base64.
+        @param type string Media type to use in link. Default: `"text/plain"`.
+
+        @return string Generated link from asset.
+
+        @shortDescription Generate link from asset data with media type.
+    */
+    generateLink: function(asset, type = "text/plain") {
+        return "data:" + type + ";base64," + asset;
+    },
+
+    /*
+        @name importer.generateLinkDOMTag
+
+        @param asset string Asset contents to use in Base64.
+        @param type string Media type to use in link. Default: `"text/css"`.
+        @param relation string Relation to use (`rel` attribute in HTML). Default: `"stylesheet"`.
+
+        @shortDescription Generate `link` DOM element from asset data with media type and relation.
+    */
+    generateLinkDOMElement: function(asset, type = "text/css", relation = "stylesheet") {
+        return dom.new("link")
+            .attribute("rel").set(relation)
+            .attribute("href").set(importer.generateLink(asset, type))
+        ;
+    }
+};
--- tests/importing/script.js
+++ tests/importing/script.js
@@ -1,4 +1,5 @@
 // @import ../../src/core/core
 // @import ../../src/dom/dom
+// @import ../../src/importer/importer
 
 console.log("Loaded all libraries.");
